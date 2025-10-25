00007b042a084ad7bbb582baf97669ec6ea757a1
LuisMonGP <antonio.perez.lamp3@gmail.com>
2025-10-25 01:11:19 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://LuisMonGP.github.io',
   integrations: [
     tailwind(),
     astroIcon({
